f725cc24beb8a9ebbe422a22a99dde25f79dfac3
Pranav Gautam <pranavgautam2902@gmail.com>
2022-01-22 21:03:41 +0530

    Area of Rectangle

---
+++ 04_Area_of_Rectangle.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter the length of the rectangle:"); 
+    scanf("%d", &a); //here we are asking the user the length of the rectangle
+    printf("Enter the width of the rectangle:");
+    scanf("%d", &b); // here we are asking the user to input the width of the recatngle
+    printf("The Area of the Rectangle whose length and width are %d, %d is %d ", a,b , a*b  );
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
